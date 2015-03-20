123f29ef20ce1ea7798765a46455a238cae61c81
Jan Korte <j.korte@me.com>
2015-03-21 00:55:53 +0100

    basic structuring works

---
+++ main.py
@@ -0,0 +1,187 @@
+from PyQt5 import QtWidgets
+import sys
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+import qrc_resources
+import model
+import subprocess
+
+
+class MainWindow(QMainWindow):
+    def _getView(self):
+        # return focused view
+        for i in range(0, self.mainSplitter.count()):
+            if self.mainSplitter.widget(i).hasFocus():
+                return self.mainSplitter.widget(i)
+        return self.mainSplitter.widget(0)
+
+    view = property(fget=_getView)
+
+    def __init__(self):
+        super(MainWindow, self).__init__()
+
+        self.model = model.TreeModel()
+        self.model.update_selection_signal[QModelIndex, QModelIndex, int].connect(self.update_selection)
+        self.model.update_selection_and_edit_signal[QModelIndex].connect(self.update_selection_and_edit)
+        self.model.layout_changed_signal.connect(self.layout_changed)
+        self.mainSplitter = QSplitter(Qt.Horizontal)
+        self.setCentralWidget(self.mainSplitter)
+
+        self.actions = list()
+
+        def add_action(name, qaction):
+            setattr(self, name, qaction)
+            self.actions.append(qaction)
+        add_action('aboutAct', QAction(self.tr('&About'), self, triggered=self.about))
+        add_action('unsplitWindowAct', QAction(QIcon(':/editedit.png'), self.tr('&Unsplit window'), self, shortcut='Ctrl+Shift+S', triggered=self.unsplit_window))
+        self.unsplitWindowAct.setEnabled(False)  # todo put in update actions
+        add_action('splitWindowAct', QAction(QIcon(':/filenew.png'), self.tr('&Split window'), self, shortcut='Ctrl+S', triggered=self.split_window))
+        add_action('editRowAction', QAction(QIcon(':/filenew.png'), self.tr('&Edit row'), self, shortcut='Tab', triggered=self.editRow))
+        add_action('deleteSelectedRowsAction', QAction(QIcon(':/filenew.png'), self.tr('&Delete selected rows'), self, shortcut='delete', triggered=self.removeSelection))
+        add_action('insertRowAction', QAction(QIcon(':/filenew.png'), self.tr('&Insert row'), self, shortcut='Return', triggered=self.insertRow))
+        add_action('insertChildAction', QAction(QIcon(':/filenew.png'), self.tr('&Insert child'), self, shortcut='Shift+Return', triggered=self.insertChild))
+        add_action('moveUpAction', QAction(QIcon(':/filenew.png'), self.tr('&Up'), self, shortcut='W', triggered=self.move_up))
+        add_action('moveDownAction', QAction(QIcon(':/filenew.png'), self.tr('&Down'), self, shortcut='S', triggered=self.move_down))
+        add_action('moveLeftAction', QAction(QIcon(':/filenew.png'), self.tr('&Left'), self, shortcut='A', triggered=self.move_left))
+        add_action('moveRightAction', QAction(QIcon(':/filenew.png'), self.tr('&Right'), self, shortcut='D', triggered=self.move_right))
+
+        self.structureMenu = self.menuBar().addMenu(self.tr('&Edit structure'))
+        self.structureMenu.addAction(self.insertChildAction)
+        self.structureMenu.addAction(self.insertRowAction)
+        self.structureMenu.addAction(self.deleteSelectedRowsAction)
+
+        self.moveMenu = self.structureMenu.addMenu(self.tr('&Move task'))
+        self.moveMenu.addAction(self.moveUpAction)
+        self.moveMenu.addAction(self.moveDownAction)
+        self.moveMenu.addAction(self.moveLeftAction)
+        self.moveMenu.addAction(self.moveRightAction)
+
+        self.taskMenu = self.menuBar().addMenu(self.tr('&Edit Task'))
+
+        self.viewMenu = self.menuBar().addMenu(self.tr('&View'))
+        self.viewMenu.addAction(self.splitWindowAct)
+        self.viewMenu.addAction(self.unsplitWindowAct)
+
+        self.helpMenu = self.menuBar().addMenu(self.tr('&Help'))
+        self.helpMenu.addAction(self.aboutAct)
+
+        # single key menu shortcuts don't work on mac, so I use this workaround: http://thebreakfastpost.com/2014/06/03/single-key-menu-shortcuts-with-qt5-on-osx/
+        if sys.platform == "darwin":
+            self.signalMapper = QSignalMapper(self)  # This class collects a set of parameterless signals, and re-emits them with a string corresponding to the object that sent the signal.
+            self.signalMapper.mapped[str].connect(self.evoke_singlekey_action)
+            for action in self.actions:
+                keySequence = action.shortcut()
+                if keySequence.count() == 1:
+                    shortcut = QShortcut(keySequence, self)
+                    shortcut.activated.connect(self.signalMapper.map)
+                    self.signalMapper.setMapping(shortcut, action.text())  # pass the action's name
+                    action.shortcut = QKeySequence()  # disable the old shortcut
+
+        self.split_window()
+        self.updateActions()
+
+        settings = QSettings()
+        self.resize(settings.value('size', QSize(400, 400)))
+        self.move(settings.value('pos', QPoint(200, 200)))
+
+    def closeEvent(self, event):
+        settings = QSettings()
+        settings.setValue('pos', self.pos())
+        settings.setValue('size', self.size())
+        self.model.updater.terminate()
+        if sys.platform == "darwin":
+            subprocess.call(['osascript', '-e', 'tell application "Apache CouchDB" to quit'])
+
+    def evoke_singlekey_action(self, action_name):  # fix shortcuts for mac
+        for action in self.actions:
+            if action.text() == action_name and action.isEnabled():
+                action.trigger()
+                break
+
+    def updateActions(self):
+        pass  # todo embed split action
+
+    # structure menu actions
+
+    def move_up(self):
+        indexes = self.view.selectionModel().selectedIndexes()
+        self.model.move_vertical(indexes, -1)
+
+    def move_down(self):
+        indexes = self.view.selectionModel().selectedIndexes()
+        self.model.move_vertical(indexes, +1)
+
+    def move_left(self):
+        self.model.move_left(self.view.selectionModel().selectedIndexes())
+
+    def move_right(self):
+        self.model.move_right(self.view.selectionModel().selectedIndexes())
+
+    def editRow(self):
+        self.view.edit(self.view.selectionModel().currentIndex())
+
+    def insertChild(self):
+        index = self.view.selectionModel().currentIndex()
+        if self.view.state() == QAbstractItemView.EditingState:
+            # commit data by changing the current selection
+            self.view.selectionModel().currentChanged.emit(index, index)
+        self.model.insertRows(0, index)
+
+    def insertRow(self):
+        index = self.view.selectionModel().currentIndex()
+        if self.view.state() == QAbstractItemView.EditingState:
+            # commit data by changing the current selection
+            self.view.selectionModel().currentChanged.emit(index, index)
+        else:
+            self.model.insertRows(index.row() + 1, index.parent())
+
+    def removeSelection(self):
+        self.view.model().removeRows(self.view.selectionModel().selectedIndexes())
+
+    # view menu actions
+
+    def split_window(self):
+        view = QTreeView()
+        view.header().hide()
+        view.setSelectionMode(QAbstractItemView.ExtendedSelection)
+        view.setModel(self.model)
+        view.selectionModel().selectionChanged.connect(self.updateActions)
+        self.mainSplitter.addWidget(view)
+        self.unsplitWindowAct.setEnabled(True)
+        self.__view = view
+
+    def unsplit_window(self):
+        index_last_widget = self.mainSplitter.count() - 1
+        self.mainSplitter.widget(index_last_widget).setParent(None)
+        if self.mainSplitter.count() == 1:
+            self.unsplitWindowAct.setEnabled(False)
+
+    # help menu actions
+
+    def about(self):
+        QMessageBox.about(self, self.tr('About'), self.tr('teeeext'))
+
+    # signals from model thread
+
+    def layout_changed(self):
+        self.model.layoutChanged.emit()
+
+    def update_selection(self, index_from, index_to, seq):
+        # Events are queued, so it may happen that the model was changed meanwhile. Then ignore the event and process the next event with the right sequence number.
+        if seq == self.model.seq:
+            selection = QItemSelection(index_from, index_to)
+            self.view.selectionModel().select(selection, QItemSelectionModel.ClearAndSelect)
+            self.view.selectionModel().setCurrentIndex(index_from, QItemSelectionModel.ClearAndSelect)
+
+    def update_selection_and_edit(self, index):
+        self.view.selectionModel().setCurrentIndex(index, QItemSelectionModel.ClearAndSelect)
+        self.view.edit(index)
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    app.setApplicationName(QApplication.translate('main', 'Todo App'))
+    app.setWindowIcon(QIcon(':/icon.png'))
+    form = MainWindow()
+    form.show()
+    app.exec_()
---
+++ model.py
@@ -0,0 +1,377 @@
+from PyQt5.QtCore import QAbstractItemModel, QModelIndex, Qt, QThread, QObject, pyqtSignal
+import couchdb
+import time
+import sys
+import subprocess
+import threading
+
+
+if sys.platform == "darwin":
+    subprocess.call(['/usr/bin/open', '/Applications/Apache CouchDB.app'])
+
+
+def get_create_db(new_db_name, db_url=None):
+    if db_url:
+        server = couchdb.Server(db_url)
+    else:
+        server = couchdb.Server()
+    try:
+        #del server[new_db_name]
+        return server, server[new_db_name]
+    except couchdb.http.ResourceNotFound:
+        new_db = server.create(new_db_name)
+        new_db['0'] = ({'text': 'root', 'children': ''})
+        print("Database does not exist. Created the database.")
+        return server, new_db
+    except couchdb.http.Unauthorized as err:
+        print(err.message)
+
+    except couchdb.http.ServerError as err:
+        print(err.message)
+
+# If a database change is arriving, we just have the id. To get the corresponding Tree_item, we store it's QModelIndex in this dict:
+id_index_dict = dict()  # New indexes are created by TreeModel.index(). That function stores the index in this dict.
+db_name = 'tree'
+server_url = 'http://192.168.178.42:5984/'
+local_server = None
+while local_server is None:  # wait until couchdb is started
+    try:
+        time.sleep(0.1)
+        local_server, db = get_create_db(db_name)
+        break
+    except:
+        pass
+
+# get_create_db(db_name, server_url)
+# local_server.replicate(db_name, server_url + db_name, continuous=True)
+# local_server.replicate(server_url + db_name, db_name, continuous=True)
+
+
+def synchronized(lock_name):
+    """
+    Synchronization decorator
+    When the user changes the TreeModel with the view, the called method (like move_left) reads some Tree_items.
+    But meanwhile an incoming, foreign database change may have deleted an affected Tree_item.
+    Therefore we use a lock variable, which is used by the TreeModel methods and by the Updater-Thread.
+    """
+    def decorator(method):
+        def synced_method(self, *args, **kws):
+            lock = getattr(self, lock_name)
+            with lock:
+                return method(self, *args, **kws)
+        return synced_method
+    return decorator
+
+
+class Updater(QThread):
+    """
+    This Thread watches the db for own and foreign changes and updates the view.
+    """
+    def __init__(self, tree_model):
+        super(QThread, self).__init__()
+        self.tree_model = tree_model
+
+    def run(self):  # this updates the view
+        all_changes = db.changes(descending=True)
+        last_seq = all_changes['results'][0]['seq']
+        changes_list = db.changes(feed='continuous', heartbeat=sys.maxsize, include_docs=True, since=last_seq)  # no need for heartbeet, because the db is local
+        for line in changes_list:
+            print(line)
+            if 'doc' in line and 'deleted' not in line:
+                db_item = line['doc']
+                item_id = db_item['_id']
+                if item_id in id_index_dict:  # update the view only if the item is already loaded
+                    with self.tree_model.lock:
+                        index = id_index_dict[item_id]
+                        item = self.tree_model.getItem(index)
+                        change_dict = db_item['change']
+
+                        def updated():
+                            item.text = db_item['text']
+                            self.tree_model.seq = line['seq']
+                            self.tree_model.update_selection_signal.emit(index, index, self.tree_model.seq)
+
+                        def added():
+                            position = change_dict['position']
+                            parentItem = self.tree_model.getItem(index)
+                            id_list = change_dict['id_list']
+                            self.tree_model.beginInsertRows(index, position, position + len(id_list) - 1)
+                            parentItem.insert_children(position, len(id_list))
+                            for i, added_item_id in enumerate(id_list):
+                                parentItem.childItems[position + i].id = added_item_id
+                                parentItem.childItems[position + i].text = db[added_item_id]['text']
+                            self.tree_model.endInsertRows()
+                            index_first_added = self.tree_model.index(position, 0, index)
+                            index_last_added = self.tree_model.index(position + len(id_list) - 1, 0, index)
+                            if change_dict['set_edit_focus']:
+                                self.tree_model.update_selection_and_edit_signal.emit(index_first_added)
+                            else:
+                                self.tree_model.seq = line['seq']
+                                self.tree_model.update_selection_signal.emit(index_first_added, index_last_added, self.tree_model.seq)
+                                self.last_selected_index = index_first_added
+
+                        def removed():
+                            self.tree_model.seq = line['seq']
+                            position = change_dict['position']
+                            count = change_dict['count']
+                            self.tree_model.beginRemoveRows(index, position, position + count - 1)
+                            item.childItems[position:position + count] = []
+                            self.tree_model.endRemoveRows()
+                            # select the item below, if existing
+                            if position == len(item.childItems):
+                                position -= 1
+                            if len(item.childItems) != 0:
+                                index_next_child = self.tree_model.index(position, 0, index)
+                                self.tree_model.update_selection_signal.emit(index_next_child, index_next_child, self.tree_model.seq)
+
+                        def moved_vertical():
+                            self.tree_model.seq = line['seq']
+                            up_or_down = change_dict['up_or_down']
+                            position = change_dict['position']
+                            count = change_dict['count']
+                            if up_or_down == -1:
+                                # if we want to move several items up, we can move the item-above below the selection instead:
+                                item.childItems.insert(position + count - 1, item.childItems.pop(position - 1))
+                                self.tree_model.index(position + count - 1, 0, index)  # calling index() refreshes the id_index_dict of that item
+                            elif up_or_down == +1:
+                                item.childItems.insert(position, item.childItems.pop(position + count))
+                                self.tree_model.index(position, 0, index)  # calling index() refreshes the id_index_dict of that item
+                            for i in range(count):
+                                index_moved_item = self.tree_model.index(position + up_or_down + i, 0, index)  # calling index() refreshes the id_index_dict of that item
+                                if i == 0:
+                                    index_first_moved_item = index_moved_item
+                            self.tree_model.layout_changed_signal.emit()
+                            self.tree_model.update_selection_signal.emit(index_first_moved_item, index_moved_item, self.tree_model.seq)
+
+                        eval(change_dict['method'] + '()')
+
+
+class Tree_item(object):
+    """
+    To understand Qt's way of building a TreeView, read:
+    http://trevorius.com/scrapbook/uncategorized/pyqt-custom-abstractitemmodel/
+    http://doc.qt.io/qt-5/qtwidgets-itemviews-editabletreemodel-example.html
+    """
+    def __init__(self, text, parent=None):
+        self.parentItem = parent
+        self.text = text
+        self.childItems = None
+        self.id = None
+
+    def deserialise_children_from_db(self):
+        if self.childItems is None:  # child_count get's called often. Improve performance by deserialising only once
+            self.childItems = []
+            children_id_list = db[self.id]['children'].split()
+            self.insert_children(0, len(children_id_list))
+            for i in range(len(children_id_list)):
+                self.childItems[i].text = db[children_id_list[i]]['text']
+                self.childItems[i].id = children_id_list[i]
+
+    def child_number(self):
+        if self.parentItem is not None:
+            return self.parentItem.childItems.index(self)
+        return 0
+
+    def insert_children(self, position, count):
+        for row in range(count):
+            item = Tree_item('', self)
+            self.childItems.insert(position, item)
+
+    def remove_children(self, position, count):
+        for row in range(count):
+            self.childItems.pop(position)
+
+
+class TreeModel(QAbstractItemModel):
+    """
+    The methods of this model changes the database only. The view gets updated by the Updater-Thread.
+    """
+    update_selection_signal = pyqtSignal(QModelIndex, QModelIndex, int)
+    update_selection_and_edit_signal = pyqtSignal(QModelIndex)
+    layout_changed_signal = pyqtSignal()
+
+    def __init__(self, parent=None):
+        super(TreeModel, self).__init__(parent)
+
+        self.lock = threading.RLock()
+
+        self.rootItem = Tree_item('root item')
+        self.rootItem.id = '0'
+        id_index_dict['0'] = QModelIndex()
+
+        self.updater = Updater(self)
+        self.updater.start()
+
+    def columnCount(self, parent):
+        return 1
+
+    def flags(self, index):
+        if not index.isValid():
+            return 0
+
+        return Qt.ItemIsEditable | Qt.ItemIsEnabled | Qt.ItemIsSelectable
+
+    def getItem(self, index):
+        if index.isValid():
+            item = index.internalPointer()
+            if item:
+                return item
+
+        return self.rootItem
+
+    @synchronized("lock")
+    def index(self, row, column, parent=QModelIndex()):
+        if parent.isValid() and parent.column() != 0:
+            return QModelIndex()
+
+        parentItem = self.getItem(parent)
+        childItem = parentItem.childItems[row]
+
+        if childItem:
+            index = self.createIndex(row, column, childItem)
+            id_index_dict[childItem.id] = index
+            return index
+        else:
+            return QModelIndex()
+
+    @synchronized("lock")
+    def parent(self, index):
+        if not index.isValid():
+            return QModelIndex()
+
+        childItem = self.getItem(index)
+        parentItem = childItem.parentItem
+
+        if parentItem == self.rootItem:
+            return QModelIndex()
+
+        return self.createIndex(parentItem.child_number(), 0, parentItem)
+
+    def rowCount(self, parent=QModelIndex()):
+        parentItem = self.getItem(parent)
+        parentItem.deserialise_children_from_db()
+        return len(parentItem.childItems)
+
+    def data(self, index, role):
+        if not index.isValid():
+            return None
+
+        if role != Qt.DisplayRole and role != Qt.EditRole:
+            return None
+
+        item = self.getItem(index)
+        return item.text
+
+    def setData(self, index, value, role=Qt.EditRole):
+        if role != Qt.EditRole:
+            return False
+
+        item = self.getItem(index)
+        db_item = db[item.id]
+        db_item['text'] = value
+        db_item['change'] = dict(method='updated')
+        db[item.id] = db_item
+        return True
+
+    @synchronized("lock")
+    def insertRows(self, position, parent, indexes=None):
+        id_list = list()
+        if indexes is None:  # used from view, create a single new row / db item
+            set_edit_focus = True
+            child_id, _ = db.save({'text': '', 'children': ''})
+            id_list.append(child_id)
+        else:  # used from move methods, add existing db items to the parent
+            set_edit_focus = False
+            for index in indexes:  # todo ginge auch über position
+                id_list.append(self.getItem(index).id)
+        parent_item_id = self.getItem(parent).id
+        db_item = db[parent_item_id]
+        children_list = db_item['children'].split()
+        children_list_new = children_list[:position] + id_list + children_list[position:]
+        db_item['children'] = ' '.join(children_list_new)
+        db_item['change'] = dict(method='added', id_list=id_list, position=position, set_edit_focus=set_edit_focus)
+        db[parent_item_id] = db_item
+        return True
+
+    @synchronized("lock")
+    def removeRows(self, indexes, delete=True, restore_selection=False):
+        for index in reversed(indexes):
+            child_item = self.getItem(index)
+            child_db_item = db.get(child_item.id)
+            if child_db_item is not None:
+                db.delete(child_db_item)
+
+                def delete_childs(item):
+                    for ch_item in item.childItems:
+                        delete_childs(ch_item)
+                        ch_db_item = db.get(ch_item.id)
+                        if ch_db_item is not None:
+                            db.delete(ch_db_item)
+
+                delete_childs(child_item)
+
+                parent_item_id = child_item.parentItem.id
+                parent_db_item = db[parent_item_id]
+                children_list = parent_db_item['children'].split()
+                parent_db_item['change'] = dict(method='removed', position=children_list.index(child_item.id), count=1)
+                children_list.remove(child_item.id)
+                parent_db_item['children'] = ' '.join(children_list)
+                db[parent_item_id] = parent_db_item
+        return True
+
+    @synchronized("lock")
+    def move_vertical(self, indexes, up_or_down):
+        # up_or_down is -1 for up and +1 for down
+        item = self.getItem(indexes[0])
+        parent_item_id = item.parentItem.id
+        db_item = db[parent_item_id]
+        children_list = db_item['children'].split()
+        old_position = children_list.index(item.id)
+        if up_or_down == -1 and old_position == 0 or up_or_down == +1 and old_position + len(indexes) - 1 == len(children_list) - 1:  # don't move if already at top or bottom
+            return
+        self.layoutAboutToBeChanged.emit()
+        if up_or_down == -1:  # if we want to move several items up, we can move the item-above below the selection instead
+            swapped_item = children_list.pop(old_position - 1)
+            swapped_item_new_position = old_position + len(indexes) - 1
+        elif up_or_down == +1:
+            swapped_item = children_list.pop(old_position + len(indexes))
+            swapped_item_new_position = old_position
+        children_list.insert(swapped_item_new_position, swapped_item)
+        db_item['children'] = ' '.join(children_list)
+        db_item['change'] = dict(method='moved_vertical', position=old_position, count=len(indexes), up_or_down=up_or_down)
+        db[parent_item_id] = db_item
+
+    @synchronized("lock")
+    def move_left(self, indexes):
+        item = self.getItem(indexes[0])
+        parent_parent_item = item.parentItem.parentItem
+        if parent_parent_item is not None:  # stop moving left if parent is root_item
+            self.remove_consecutive_rows_from_parent(indexes)
+
+            # insert as a child of the parent's parent
+            parent_parent_item_index = id_index_dict[parent_parent_item.id]
+            position = item.parentItem.child_number() + 1
+            self.insertRows(position, parent_parent_item_index, indexes)
+
+    @synchronized("lock")
+    def move_right(self, indexes):
+        # insert as a child of the sibling above
+        item = self.getItem(indexes[0])
+        childNumber = item.child_number()
+        if childNumber != 0:  # stop moving right if the moving item is the top item
+            self.remove_consecutive_rows_from_parent(indexes)  # we have to restore the selection of the moved item, because removeRow() discards it
+
+            sibling_index = self.index(childNumber - 1, 0, self.parent(indexes[0]))
+            last_childnr_of_sibling = len(item.parentItem.childItems[childNumber - 1].childItems)
+            self.insertRows(last_childnr_of_sibling, sibling_index, indexes)  # the solution 'delete and then insert the moving item' produces a flickering, so we insert first
+
+    def remove_consecutive_rows_from_parent(self, indexes):  # just for moving
+        child_item = self.getItem(indexes[0])
+        parent_item_id = child_item.parentItem.id
+        parent_db_item = db[parent_item_id]
+        children_list = parent_db_item['children'].split()
+        position = children_list.index(child_item.id)
+        parent_db_item['change'] = dict(method='removed', position=position, count=len(indexes))
+        children_list[position:position + len(indexes)] = []
+        parent_db_item['children'] = ' '.join(children_list)
+        db[parent_item_id] = parent_db_item
