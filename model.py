a877c58df2d3dd61896a9410a95936b876e79fef
Jan Korte <j.korte@me.com>
2015-03-21 03:26:19 +0100

    more object oriented design

--- model.py
+++ model.py
@@ -6,46 +6,6 @@ import subprocess
 import threading
 import socket
 
-if sys.platform == "darwin":
-    subprocess.call(['/usr/bin/open', '/Applications/Apache CouchDB.app'])
-
-
-def get_create_db(new_db_name, db_url=None):
-    if db_url:
-        server = couchdb.Server(db_url)
-    else:
-        server = couchdb.Server()
-    try:
-        del server[new_db_name]
-        return server, server[new_db_name]
-    except couchdb.http.ResourceNotFound:
-        new_db = server.create(new_db_name)
-        new_db['0'] = ({'text': 'root', 'children': '', 'user': ''})
-        print("Database does not exist. Created the database.")
-        return server, new_db
-    except couchdb.http.Unauthorized as err:
-        print(err.message)
-
-    except couchdb.http.ServerError as err:
-        print(err.message)
-
-# If a database change is arriving, we just have the id. To get the corresponding Tree_item, we store it's QModelIndex in this dict:
-id_index_dict = dict()  # New indexes are created by TreeModel.index(). That function stores the index in this dict.
-db_name = 'tree'
-server_url = 'http://192.168.178.42:5984/'
-local_server = None
-while local_server is None:  # wait until couchdb is started
-    try:
-        time.sleep(0.1)
-        local_server, db = get_create_db(db_name)
-        break
-    except:
-        pass
-
-# get_create_db(db_name, server_url)
-# local_server.replicate(db_name, server_url + db_name, continuous=True)
-# local_server.replicate(server_url + db_name, db_name, continuous=True)
-
 
 def synchronized(lock_name):
     """
@@ -76,17 +36,17 @@ class Updater(QThread):
         self.tree_model = tree_model
 
     def run(self):  # this updates the view
-        all_changes = db.changes(descending=True)
+        all_changes = self.tree_model.db.changes(descending=True)
         last_seq = all_changes['results'][0]['seq']
-        changes_list = db.changes(feed='continuous', heartbeat=sys.maxsize, include_docs=True, since=last_seq)  # no need for heartbeet, because the db is local
+        changes_list = self.tree_model.db.changes(feed='continuous', heartbeat=sys.maxsize, include_docs=True, since=last_seq)  # no need for heartbeet, because the db is local
         for line in changes_list:
             print(line)
             if 'doc' in line and 'deleted' not in line:
                 db_item = line['doc']
                 item_id = db_item['_id']
-                if item_id in id_index_dict:  # update the view only if the item is already loaded
+                if item_id in self.tree_model.id_index_dict:  # update the view only if the item is already loaded
                     with self.tree_model.lock:
-                        index = id_index_dict[item_id]
+                        index = self.tree_model.id_index_dict[item_id]
                         item = self.tree_model.getItem(index)
                         change_dict = db_item['change']
                         my_edit = db_item['user'] == socket.gethostname()
@@ -105,7 +65,7 @@ class Updater(QThread):
                             parentItem.insert_children(position, len(id_list))
                             for i, added_item_id in enumerate(id_list):
                                 parentItem.childItems[position + i].id = added_item_id
-                                parentItem.childItems[position + i].text = db[added_item_id]['text']
+                                parentItem.childItems[position + i].text = self.tree_model.db[added_item_id]['text']
                             self.tree_model.endInsertRows()
                             if my_edit:
                                 index_first_added = self.tree_model.index(position, 0, index)
@@ -142,12 +102,12 @@ class Updater(QThread):
                             if up_or_down == -1:
                                 # if we want to move several items up, we can move the item-above below the selection instead:
                                 item.childItems.insert(position + count - 1, item.childItems.pop(position - 1))
-                                self.tree_model.index(position + count - 1, 0, index)  # calling index() refreshes the id_index_dict of that item
+                                self.tree_model.index(position + count - 1, 0, index)  # calling index() refreshes the self.tree_model.id_index_dict of that item
                             elif up_or_down == +1:
                                 item.childItems.insert(position, item.childItems.pop(position + count))
-                                self.tree_model.index(position, 0, index)  # calling index() refreshes the id_index_dict of that item
+                                self.tree_model.index(position, 0, index)  # calling index() refreshes the self.tree_model.id_index_dict of that item
                             for i in range(count):
-                                index_moved_item = self.tree_model.index(position + up_or_down + i, 0, index)  # calling index() refreshes the id_index_dict of that item
+                                index_moved_item = self.tree_model.index(position + up_or_down + i, 0, index)  # calling index() refreshes the self.tree_model.id_index_dict of that item
                                 if i == 0:
                                     index_first_moved_item = index_moved_item
                             self.tree_model.layout_changed_signal.emit()
@@ -169,15 +129,6 @@ class Tree_item(object):
         self.text = text
         self.childItems = None
         self.id = None
-
-    def deserialise_children_from_db(self):
-        if self.childItems is None:  # child_count get's called often. Improve performance by deserialising only once
-            self.childItems = []
-            children_id_list = db[self.id]['children'].split()
-            self.insert_children(0, len(children_id_list))
-            for i in range(len(children_id_list)):
-                self.childItems[i].text = db[children_id_list[i]]['text']
-                self.childItems[i].id = children_id_list[i]
 
     def child_number(self):
         if self.parentItem is not None:
@@ -207,9 +158,48 @@ class TreeModel(QAbstractItemModel):
 
         self.lock = threading.RLock()
 
+        if sys.platform == "darwin":
+            subprocess.call(['/usr/bin/open', '/Applications/Apache CouchDB.app'])
+
+        def get_create_db(new_db_name, db_url=None):
+            if db_url:
+                server = couchdb.Server(db_url)
+            else:
+                server = couchdb.Server()
+            try:
+                del server[new_db_name]
+                return server, server[new_db_name]
+            except couchdb.http.ResourceNotFound:
+                new_db = server.create(new_db_name)
+                new_db['0'] = ({'text': 'root', 'children': '', 'user': ''})
+                print("Database does not exist. Created the database.")
+                return server, new_db
+            except couchdb.http.Unauthorized as err:
+                print(err.message)
+
+            except couchdb.http.ServerError as err:
+                print(err.message)
+
+        # If a database change is arriving, we just have the id. To get the corresponding Tree_item, we store it's QModelIndex in this dict:
+        self.id_index_dict = dict()  # New indexes are created by TreeModel.index(). That function stores the index in this dict.
+        db_name = 'tree'
+        server_url = 'http://192.168.178.42:5984/'
+        local_server = None
+        while local_server is None:  # wait until couchdb is started
+            try:
+                time.sleep(0.1)
+                local_server, self.db = get_create_db(db_name)
+                break
+            except:
+                pass
+
+        # get_create_db(db_name, server_url)
+        # local_server.replicate(db_name, server_url + db_name, continuous=True)
+        # local_server.replicate(server_url + db_name, db_name, continuous=True)
+
         self.rootItem = Tree_item('root item')
         self.rootItem.id = '0'
-        id_index_dict['0'] = QModelIndex()
+        self.id_index_dict['0'] = QModelIndex()
 
         self.updater = Updater(self)
         self.updater.start()
@@ -241,7 +231,7 @@ class TreeModel(QAbstractItemModel):
 
         if childItem:
             index = self.createIndex(row, column, childItem)
-            id_index_dict[childItem.id] = index
+            self.id_index_dict[childItem.id] = index
             return index
         else:
             return QModelIndex()
@@ -259,9 +249,16 @@ class TreeModel(QAbstractItemModel):
 
         return self.createIndex(parentItem.child_number(), 0, parentItem)
 
+    # also deserialises children from the db
     def rowCount(self, parent=QModelIndex()):
         parentItem = self.getItem(parent)
-        parentItem.deserialise_children_from_db()
+        if parentItem.childItems is None:  # child_count get's called often. Improve performance by deserialising only once
+            parentItem.childItems = []
+            children_id_list = self.db[parentItem.id]['children'].split()
+            parentItem.insert_children(0, len(children_id_list))
+            for i in range(len(children_id_list)):
+                parentItem.childItems[i].text = self.db[children_id_list[i]]['text']
+                parentItem.childItems[i].id = children_id_list[i]
         return len(parentItem.childItems)
 
     def data(self, index, role):
@@ -279,57 +276,57 @@ class TreeModel(QAbstractItemModel):
             return False
 
         item = self.getItem(index)
-        db_item = db[item.id]
+        db_item = self.db[item.id]
         db_item['text'] = value
 
         db_item['change'] = dict(method='updated', user=socket.gethostname())
-        db[item.id] = db_item
+        self.db[item.id] = db_item
         return True
 
     @synchronized("lock")
     def insertRows(self, position, parent, indexes=None):
         id_list = list()
-        if indexes is None:  # used from view, create a single new row / db item
+        if indexes is None:  # used from view, create a single new row / self.db item
             set_edit_focus = True
-            child_id, _ = db.save({'text': '', 'children': '', 'user': ''})
+            child_id, _ = self.db.save({'text': '', 'children': '', 'user': ''})
             id_list.append(child_id)
         else:  # used from move methods, add existing db items to the parent
             set_edit_focus = False
             for index in indexes:  # todo ginge auch über position
                 id_list.append(self.getItem(index).id)
         parent_item_id = self.getItem(parent).id
-        db_item = db[parent_item_id]
+        db_item = self.db[parent_item_id]
         children_list = db_item['children'].split()
         children_list_new = children_list[:position] + id_list + children_list[position:]
         db_item['children'] = ' '.join(children_list_new)
         db_item['change'] = dict(method='added', id_list=id_list, position=position, set_edit_focus=set_edit_focus, user=socket.gethostname())
-        db[parent_item_id] = db_item
+        self.db[parent_item_id] = db_item
         return True
 
     @synchronized("lock")
     def removeRows(self, indexes, delete=True, restore_selection=False):
         for index in reversed(indexes):
             child_item = self.getItem(index)
-            child_db_item = db.get(child_item.id)
+            child_db_item = self.db.get(child_item.id)
             if child_db_item is not None:
-                db.delete(child_db_item)
+                self.db.delete(child_db_item)
 
                 def delete_childs(item):
                     for ch_item in item.childItems:
                         delete_childs(ch_item)
-                        ch_db_item = db.get(ch_item.id)
+                        ch_db_item = self.db.get(ch_item.id)
                         if ch_db_item is not None:
-                            db.delete(ch_db_item)
+                            self.db.delete(ch_db_item)
 
                 delete_childs(child_item)
 
                 parent_item_id = child_item.parentItem.id
-                parent_db_item = db[parent_item_id]
+                parent_db_item = self.db[parent_item_id]
                 children_list = parent_db_item['children'].split()
                 parent_db_item['change'] = dict(method='removed', position=children_list.index(child_item.id), count=1, user=socket.gethostname())
                 children_list.remove(child_item.id)
                 parent_db_item['children'] = ' '.join(children_list)
-                db[parent_item_id] = parent_db_item
+                self.db[parent_item_id] = parent_db_item
         return True
 
     @synchronized("lock")
@@ -337,7 +334,7 @@ class TreeModel(QAbstractItemModel):
         # up_or_down is -1 for up and +1 for down
         item = self.getItem(indexes[0])
         parent_item_id = item.parentItem.id
-        db_item = db[parent_item_id]
+        db_item = self.db[parent_item_id]
         children_list = db_item['children'].split()
         old_position = children_list.index(item.id)
         if up_or_down == -1 and old_position == 0 or up_or_down == +1 and old_position + len(indexes) - 1 == len(children_list) - 1:  # don't move if already at top or bottom
@@ -352,7 +349,7 @@ class TreeModel(QAbstractItemModel):
         children_list.insert(swapped_item_new_position, swapped_item)
         db_item['children'] = ' '.join(children_list)
         db_item['change'] = dict(method='moved_vertical', position=old_position, count=len(indexes), up_or_down=up_or_down, user=socket.gethostname())
-        db[parent_item_id] = db_item
+        self.db[parent_item_id] = db_item
 
     @synchronized("lock")
     def move_left(self, indexes):
@@ -362,7 +359,7 @@ class TreeModel(QAbstractItemModel):
             self.remove_consecutive_rows_from_parent(indexes)
 
             # insert as a child of the parent's parent
-            parent_parent_item_index = id_index_dict[parent_parent_item.id]
+            parent_parent_item_index = self.id_index_dict[parent_parent_item.id]
             position = item.parentItem.child_number() + 1
             self.insertRows(position, parent_parent_item_index, indexes)
 
@@ -381,10 +378,10 @@ class TreeModel(QAbstractItemModel):
     def remove_consecutive_rows_from_parent(self, indexes):  # just for moving
         child_item = self.getItem(indexes[0])
         parent_item_id = child_item.parentItem.id
-        parent_db_item = db[parent_item_id]
+        parent_db_item = self.db[parent_item_id]
         children_list = parent_db_item['children'].split()
         position = children_list.index(child_item.id)
         parent_db_item['change'] = dict(method='removed', position=position, count=len(indexes), user=socket.gethostname())
         children_list[position:position + len(indexes)] = []
         parent_db_item['children'] = ' '.join(children_list)
-        db[parent_item_id] = parent_db_item
+        self.db[parent_item_id] = parent_db_item
