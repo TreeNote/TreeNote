a0ac702eda64f7a7aecc83d1f7b35b80fc202201
Jan <j.korte@me.com>
2017-02-16 11:42:24 +0100

    Reverted 'workaround for current pyqt bug about layoutChanged signals'
    ... because pyqt5.8 is released and the workaround caused visual glitches and crashes from vertical movement of items.

--- treenote/model.py
+++ treenote/model.py
@@ -6,7 +6,8 @@ import re
 import sys
 from xml.sax.saxutils import escape
 
-from PyQt5.QtCore import QAbstractItemModel, QModelIndex, QSortFilterProxyModel, QSize, Qt, QEvent
+from PyQt5.QtCore import QAbstractItemModel, QModelIndex, QSortFilterProxyModel, QSize, Qt, QEvent, \
+    QPersistentModelIndex
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 
@@ -482,7 +483,7 @@ class TreeModel(QAbstractItemModel):
                     count = len(indexes)
                     old_child_number = item.child_number()
 
-                    self.model.layoutAboutToBeChanged.emit()
+                    self.model.layoutAboutToBeChanged.emit([QPersistentModelIndex(parent_index)])
 
                     index_first_moved_item = self.model.index(old_child_number, 0, parent_index)
                     index_last_moved_item = self.model.index(old_child_number + count - 1, 0, parent_index)
@@ -510,7 +511,7 @@ class TreeModel(QAbstractItemModel):
                     self.model.changePersistentIndex(index_last_moved_item, index_last_moved_item_new)
                     self.model.changePersistentIndex(index_moving_item, index_moving_item_new)
 
-                    self.model.layoutChanged.emit()
+                    self.model.layoutChanged.emit([QPersistentModelIndex(parent_index)])
 
                     self.model.main_window.select_from_to(index_first_moved_item_new, index_last_moved_item_new)
                     for row_index in self.model.main_window.focused_column().view.selectionModel().selectedRows():
