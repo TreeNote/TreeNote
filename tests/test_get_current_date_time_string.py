ae1db687ffb3d5691e8b27b8f18ae2801de33230
Markus Hackspacher <hackspacher@gmx.de>
2018-05-22 22:30:15 +0200

    regex escaping

--- tests/test_get_current_date_time_string.py
+++ tests/test_get_current_date_time_string.py
@@ -12,9 +12,9 @@ class TestGetCurrentDateTimeString(TestCase):
         """check if date_time_string in the format like 2017-02-13-22-04-31-908
         and check is the string unique"""
         date_time_string = treenote.main.get_current_date_time_string()
-        self.assertRegex(date_time_string, "2\d\d\d")  # \d = one digit 0-9
+        self.assertRegex(date_time_string, r"2\d\d\d")  # \d = one digit 0-9
         self.assertRegex(date_time_string,
-                         "2\d\d\d-\d\d-\d\d-\d\d-\d\d-\d\d-\d\d\d")
+                         r"2\d\d\d-\d\d-\d\d-\d\d-\d\d-\d\d-\d\d\d")
         time.sleep(.1)
         date_time_string_new = treenote.main.get_current_date_time_string()
         self.assertNotEqual(date_time_string, date_time_string_new)
